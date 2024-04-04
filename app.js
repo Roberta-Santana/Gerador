e5a0833103da5087a5f57fd7ac8b2f7adb7466a8
Roberta Santana <g.roberta.s@hotmail.com>
2024-04-04 19:54:48 -0300

    Gerando um número  inteiro aleatório entre dois valores (min, max)

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ let listaNumerosEntrada = [];
 let listaNumeroSaida = [];
 let limiteMaximo = 14;
 
+function gerarNumeroInteiroAleatorio(min, max){ /* Gerando um n inteiro aleatório entre dois valores (min, max) */
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max - min + 1 ))+min;
+}
+
 function gerarNumeroAleatorio(){
 for(var i=0; i < 5 ;i++){
-    let numeroLimite = parseInt(Math.random()*(15-0));
+    let numeroLimite = gerarNumeroInteiroAleatorio(0,15);
     numeroEntrada = parseInt(Math.random()*7);
     while(listaNumerosEntrada.includes(numeroEntrada)){
         numeroEntrada = parseInt(Math.random()*7);
     } 
+
     listaNumerosEntrada.push(numeroEntrada);
     numeroSaida=numeroLimite-numeroEntrada;
+
+    if(numeroSaida<0){
+        numeroEntrada = parseInt(Math.random()*7);
+    }
     listaNumeroSaida.push(numeroSaida);
 }
 var paragrafoEntrada = document.getElementById('entrada__valores');
