23ec1cec5a23b78c9915d891a99f8c845491b4b9
Roberta Santana <g.roberta.s@hotmail.com>
2024-04-04 16:25:36 -0300

    numero aleatórios dentro de um intervalo

---
+++ app.js
@@ -0,0 +1,26 @@
+let numeroEntrada=0;
+let numeroSaida=0;
+let listaNumerosEntrada = [];
+let listaNumeroSaida = [];
+let limiteMaximo = 14;
+
+/* let numeroSecreto = gerarNumeroAleatorio(); */
+
+function gerarNumeroAleatorio(){
+for(var i=0; i < 5 ;i++){
+    let numeroLimite = parseInt(Math.random()*(15-0));
+    numeroEntrada = parseInt(Math.random()*7);
+    while(listaNumerosEntrada.includes(numeroEntrada)){
+        numeroEntrada = parseInt(Math.random()*7);
+    } 
+    listaNumerosEntrada.push(numeroEntrada);
+    
+    numeroSaida=numeroLimite-numeroEntrada;
+    listaNumeroSaida.push(numeroSaida);
+}
+
+var paragrafoEntrada = document.getElementById('entrada__valores');
+paragrafoEntrada.innerHTML = listaNumerosEntrada.join(' , ');   
+var paragrafoSaida = document.getElementById('saida__valores');
+paragrafoSaida.innerHTML = listaNumeroSaida.join(' , ');
+}
